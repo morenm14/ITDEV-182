8d3bd272747937d6e20c566e7b9aae5e0cce7b20
morenm14 <morenm14@gmatc.matc.edu>
2022-01-24 16:52:50 -0600

    added state to app timers

--- TimeTracking/App.js
+++ TimeTracking/App.js
@@ -1,31 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import uuidv4 from 'uuid/v4';
 import {SafeAreaView, ScrollView, StyleSheet, Text, View} from 'react-native';
 import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
 
 const App = () => {
+  const [timers, setTimers] = useState([
+    {
+      title: 'Mow the lawn',
+      project: 'House chores',
+      id: uuidv4,
+      elapsed: 5456099,
+      isRunning: true,
+    },
+    {
+      title: 'Bake squash',
+      project: 'Kitchen chores',
+      id: uuidv4,
+      elapsed: 5456099,
+      isRunning: false,
+    },
+  ]);
   return (
     <View style={styles.appContainer}>
       <View style={styles.titleContainer}>
         <Text style={styles.title}>Timers</Text>
       </View>
       <SafeAreaView>
-        <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <ScrollView
+          style={styles.timerList}
+          contentInsetAdjustmentBehavior="automatic">
           <ToggleableTimerForm isOpen={false} />
-          <EditableTimer
-            id="1"
-            title="Mow the lawn"
-            project="House chores"
-            elapsed="3890985"
-            isRunning
-          />
-          <EditableTimer
-            id="2"
-            title="Bake Squash"
-            project="Kitchen chores"
-            elapsed="3890985"
-            editFormOpen
-          />
+          {timers.map(({title, project, id, elapsed, isRunning}) => (
+            <EditableTimer
+              key={id}
+              id={id}
+              title={title}
+              project={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+            />
+          ))}
         </ScrollView>
       </SafeAreaView>
     </View>
@@ -37,7 +52,7 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   titleContainer: {
-    paddingTop: 60,
+    paddingTop: 45,
     paddingBottom: 15,
     borderBottomWidth: 1,
     borderBottomColor: '#D6D7DA',
@@ -46,6 +61,9 @@ const styles = StyleSheet.create({
     fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  timerList: {
+    paddingBottom: 15,
   },
 });
 

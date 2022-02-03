156e322d51d1188103968624452992c10592440f
morenm14 <morenm14@gmatc.matc.edu>
2022-02-03 11:40:30 -0600

    initial commit

---
+++ ImageFeed/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <ScrollView></ScrollView>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;

98e2248c6850f1bf8118046cf9e22ceb2d2aadb1
morenm14 <morenm14@gmatc.matc.edu>
2022-02-23 14:22:29 -0600

    created message utils

--- messaging/utils/MessageUtils.js
+++ messaging/utils/MessageUtils.js
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 
-const MessageShape = PropTypes.shape({
+export const MessageShape = PropTypes.shape({
   id: PropTypes.number.isRequired,
   type: PropTypes.oneOf(['text', 'image', 'location']),
   text: PropTypes.string,
@@ -10,3 +10,34 @@ const MessageShape = PropTypes.shape({
     longitude: PropTypes.number.isRequired,
   }),
 });
+
+let messageId = 0;
+
+function getNextId() {
+  messageId += 1;
+  return messageId;
+}
+
+export function createTextMessage(text) {
+  return {
+    type: 'text',
+    id: getNextId(),
+    text: text,
+  };
+}
+
+export function createImageMessage(uri) {
+  return {
+    type: 'image',
+    id: getNextId(),
+    uri: uri,
+  };
+}
+
+export function createLocationMessage(coordinate) {
+  return {
+    type: 'location',
+    id: getNextId(),
+    coordinate: coordinate,
+  };
+}

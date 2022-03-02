b5649121b19f7e37b8b31f2594e318f48d49b047
morenm14 <morenm14@gmatc.matc.edu>
2022-03-02 13:17:21 -0600

    addedphotos from camera roll

--- messaging/components/ImageGrid.js
+++ messaging/components/ImageGrid.js
@@ -1,7 +1,7 @@
 import {StyleSheet, TouchableOpacity, Image} from 'react-native';
 import CameraRoll from '@react-native-community/cameraroll';
 import PropTypes from 'prop-types';
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import Grid from './Grid';
 
 const ImageGrid = () => {
@@ -11,6 +11,22 @@ const ImageGrid = () => {
     {uri: 'https://picsum.photos/600/600?image=30'},
     {uri: 'https://picsum.photos/600/600?image=40'},
   ]);
+
+  useEffect(() => {
+    getImages();
+  }, []);
+
+  const getImages = async () => {
+    const results = await CameraRoll.getPhotos({
+      first: 20,
+      assetType: 'Photos',
+    });
+    const {edges} = results;
+    const loadedImages = edges.map(item => {
+      return {uri: item.node.image.uri};
+    });
+    setImages([...images, ...loadedImages]);
+  };
 
   const keyExtractor = ({uri}) => uri;
 

54ae75b06a61c931f533b3da25fc047d308fd750
morenm14 <morenm14@gmatc.matc.edu>
2022-04-12 17:38:38 -0500

    initialcommit. Create project and created login component and connected to api

--- MusiK/App.js
+++ MusiK/App.js
@@ -1,12 +1,12 @@
-import React from 'react'
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
+import Login from './components/Login'
 
 export default function App() {
     return (
         <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
             <StatusBar style="auto" />
+            <Login />
         </View>
     )
 }
@@ -18,5 +18,4 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
     },
-    
 })
